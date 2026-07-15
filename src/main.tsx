36ec0e1c5b3069cb54f8fc6dc9399973ae1588ec
Ibrahim Adisa Abdulsalam <salamiibrahim57@gmail.com>
2026-07-15 20:03:10 +0100

    Add system theme support with next-themes + dark mode CSS vars

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'next-themes'
 import './styles/index.css'
 import App from './app/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 )
